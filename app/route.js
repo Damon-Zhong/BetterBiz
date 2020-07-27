bea5a78531ac8477b746e11f660f9adc73143997
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-07-27 15:05:09 -0400

    Created basic prject file structure

---
+++ app/route.js
@@ -0,0 +1,23 @@
+function router( app ){
+    // app.get('/api/words', async function(req, res) {
+    //     console.log( '[GET] getting word')
+
+    //     res.send( list )
+    // })
+
+    // app.post( '/api/words', async function( req, res ){
+    //     console.log( '[POST /api/words] req.body: ', req.body )
+
+    //     const saveResult = { _id: true }
+    //     console.log( '[POST /api/dogs] saveResult: ', saveResult )
+
+    //     if( saveResult._id ){
+    //         res.send( { status: true, message: 'Dog saved' } )
+    //     } else {
+    //         res.send( { status: false, message: 'Someting went wong' } )
+    //     }
+
+    // })
+}
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,25 @@
+require( 'dotenv' ).config()
+
+const express = require("express");
+const router = require("./app/route");
+
+const app = express();
+// handlebars initialization
+
+// for parsing incoming POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// for routes
+router(app)
+
+// for serving media assets
+app.use( express.static('public') )
+
+app.listen(PORT, function() {
+    console.log( `Listening on port: ${PORT}` );
+})
