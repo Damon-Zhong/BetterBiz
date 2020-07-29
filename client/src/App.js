27fc8164d23880e04c809765a3ff86776cec94d4
Etam <etammao@gmail.com>
2020-07-29 16:31:48 -0400

    bug fixing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import logo from './logo.svg';
-// import './App.css';
-=======
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Services from "./components/Services";
@@ -14,110 +9,10 @@ import Submit from "./components/Submit";
 import AboutUs from "./components/AboutUs";
 import SignUp from "./components/SignUp";
 import Map from "./components/Map";
->>>>>>> develop
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                sign up/log in<span class="sr-only">(current)</span>
-              </a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              sign up/log in
-            </button>
-          </form>
-        </div>
-      </nav>
-
-      <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Fluid jumbotron</h1>
-          <p class="lead">
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            
-          </div>
-          <div class="col-sm-4">.col-sm-4</div>
-          <div class="col-sm-4">.col-sm-4</div>
-        </div>
-      </div>
-=======
       <Router>
         <Navbar />
         <Route exact path="/">
@@ -135,7 +30,6 @@ function App() {
         </Route>
       </Router>
       <Map />
->>>>>>> develop
     </>
   );
 }
