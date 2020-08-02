317c72c0fadc420606685ebf000d04290cd5d45b
Etam <etammao@gmail.com>
2020-08-02 15:26:28 -0400

    Fixed change pwd, added styles to headerBtn Login/LogoutBtn

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,50 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = (props) => {
   const signOut = () => {
-    window.localStorage.clear()
-    window.location.pathname = '/'
-  }
-  return(
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link to='/'>BetterBiz</Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    window.localStorage.clear();
+    window.location.pathname = "/";
+  };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/">BetterBiz</Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="/">
-            Home {window.location === '/' ? <span className="sr-only">(current)</span> : ''}
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/submit">
-                   Submit Business {window.location === '/submit' ? <span className="sr-only">(current)</span> : ''}
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">
-                   About Us {window.location === '/about' ? <span className="sr-only">(current)</span> : ''}
-          </a>
-        </li>
-      </ul>
-      {props.currUser ? 
-    (<span>Welcome Back! {props.currUser.firstName}<a href='/' onClick={signOut}>Log Out</a></span>):
-    (<Link to='/account'>Login | Signup</Link>)}
-    </div>
-  </nav>
-  )
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home{" "}
+              {window.location === "/" ? (
+                <span className="sr-only">(current)</span>
+              ) : (
+                ""
+              )}
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/submit">
+              Submit Business{" "}
+              {window.location === "/submit" ? (
+                <span className="sr-only">(current)</span>
+              ) : (
+                ""
+              )}
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/about">
+              About Us{" "}
+              {window.location === "/about" ? (
+                <span className="sr-only">(current)</span>
+              ) : (
+                ""
+              )}
+            </a>
+          </li>
+        </ul>
+        {props.currUser ? (
+          <span>
+            <p className="welcomeName">
+              Welcome Back! {props.currUser.firstName}
+            </p>
+            <a className="logoutBtn" href="/" onClick={signOut}>
+              Log Out
+            </a>
+          </span>
+        ) : (
+          <Link className="loginBtn" to="/account">
+            Login | Signup
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
