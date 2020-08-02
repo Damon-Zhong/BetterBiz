ab87bd587a51e46880f1adee76dd083ae4f66d6b
Etam <etammao@gmail.com>
2020-08-02 15:42:40 -0400

    styled AboutUs a bit

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -1,35 +1,36 @@
-import React from "react"; 
+import React from "react";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   return (
     <section className="content-section" id="portfolio">
       <div className="container">
         <div className="content-section-heading text-center">
-          <h3 className="text-secondary mb-0">About us</h3> 
-          <h2 className="mb-5">Our Story </h2> 
-          <div className="container text-center my-auto">  
-          <div className="h2">BetterBiz is a plateform that ...</div> 
- 
-
-
-      </div>
+          <h3 className="text-secondary mb-0">About us</h3>
+          <h2 className="mb-5">Our Story </h2>
+          <div className="container text-center my-auto">
+            <div className="h2">BetterBiz is a plateform that ...</div>
+          </div>
 
           <h2 className="mb-5">Our Team </h2>
         </div>
         <div className="row no-gutters">
           <div className="col-lg-6">
-            <a className="portfolio-item" href="#!">
+            <a className="portfolio-item members" href="#!">
               <div className="caption">
                 <div className="caption-content">
                   <div className="h2">Marcel</div>
-                  <p className="mb-0">Hi, I'm Marcel, a developer and mobility specialist with a background in strategy and enterprise software.</p>
+                  <p className="mb-0">
+                    Hi, I'm Marcel, a developer and mobility specialist with a
+                    background in strategy and enterprise software.
+                  </p>
                 </div>
               </div>
               <img className="img-fluid" src="./images/black.jpg" alt="" />
             </a>
           </div>
           <div className="col-lg-6">
-            <a className="portfolio-item" href="#!">
+            <a className="portfolio-item members" href="#!">
               <div className="caption">
                 <div className="caption-content">
                   <div className="h2"> Damon</div>
@@ -44,7 +45,7 @@ const AboutUs = () => {
             </a>
           </div>
           <div className="col-lg-6">
-            <a className="portfolio-item" href="#!">
+            <a className="portfolio-item members" href="#!">
               <div className="caption">
                 <div className="caption-content">
                   <div className="h2">Etam</div>
@@ -55,7 +56,7 @@ const AboutUs = () => {
             </a>
           </div>
           <div className="col-lg-6">
-            <a className="portfolio-item" href="#!">
+            <a className="portfolio-item members" href="#!">
               <div className="caption">
                 <div className="caption-content">
                   <div className="h2">Sadia</div>
@@ -67,7 +68,7 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-    </section> 
+    </section>
   );
 };
 
