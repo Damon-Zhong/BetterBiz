cd05dd3783d989ff817c20f00997c648b93eddd6
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-08-04 18:08:08 -0400

    Added new checkboxes to submit business form

--- client/src/pages/submitBus.js
+++ client/src/pages/submitBus.js
@@ -13,9 +13,22 @@ const SubmitBus = () => {
     city: "",
     country: "",
     postalCode: "",
-    attributes: "",
+    highlight: [],
   });
   const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleHighlightChange = (event) => {
+    const {value} = event.currentTarget;
+    if(busData.highlight.includes(value)){
+      const indexToRemove = busData.highlight.findIndex(highlight =>{
+      return highlight === value;
+      })
+      const result = [...busData.highlight.slice(0, indexToRemove), ...busData.highlight.slice(indexToRemove + 1)]
+      setBusData({...busData, highlight: result});
+    } else {
+      setBusData({...busData, highlight: [...busData.highlight, value]});
+    }
+  }
 
   const handleInputChange = (event) => {
     const { id, value } = event.target;
@@ -42,22 +55,8 @@ const SubmitBus = () => {
       <div className="mt-5 container">
         <form>
           <div className="form-group">
-            <label htmlFor="busType">Type of Business</label>
-            <select
-              onChange={handleInputChange}
-              value={busData.busType}
-              className="form-control"
-              id="busType"
-              required
-            >
-              <option>Restaurant</option>
-              <option>Barber</option>
-              <option>Coffee Shop</option>
-              <option>Others</option>
-              <option>Others</option>
-            </select>
 
-            <label htmlFor="name">Business Name</label>
+            <label className="mt-3 formLabel" htmlFor="name">Business Name</label>
             <input
               onChange={handleInputChange}
               value={busData.name}
@@ -68,17 +67,7 @@ const SubmitBus = () => {
               required
             />
 
-            <label htmlFor="address">Business Address</label>
-            <input
-              onChange={handleInputChange}
-              value={busData.address}
-              type="text"
-              className="form-control"
-              id="address"
-              required
-            />
-
-            <label htmlFor="city">City</label>
+            <label className="mt-3 formLabel" htmlFor="city">City</label>
             <input
               onChange={handleInputChange}
               value={busData.city}
@@ -88,35 +77,52 @@ const SubmitBus = () => {
               name="city"
             />
 
-            <label htmlFor="country">Country</label>
-            <input
+            <label className="mt-3 formLabel" htmlFor="busType">Type of Business</label>
+            <select
               onChange={handleInputChange}
-              value={busData.country}
-              type="text"
+              value={busData.busType}
               className="form-control"
-              id="country"
-              name="country"
-            />
+              id="busType"
+              required
+            >
+              <option>Food</option>
+              <option>Service</option>
+              <option>Office</option>
+              <option>Culture</option>
+              <option>Other</option>
+            </select>
 
-            <label htmlFor="postalCode">Postal Code</label>
-            <input
-              onChange={handleInputChange}
-              value={busData.postalcode}
-              type="text"
-              className="form-control"
-              id="postalCode"
-              name="postalCode"
-            />
-
-            <label htmlFor="attributes">Business Attributes</label>
-            <input
-              onChange={handleInputChange}
-              value={busData.attributes}
-              type="text"
-              className="form-control"
-              id="attributes"
-              name="attributes"
-            />
+            <label className="mt-3 formLabel" htmlFor="attributes">What makes this business special?</label>
+            <div class="form-check">
+              <input class="form-check-input" onClick={handleHighlightChange} type="checkbox" value="Black" id="black-owned-check"/>
+              <label class="form-check-label" for="black-owned-check">
+                🖤 Black-owned
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" onClick={handleHighlightChange} type="checkbox" value="LGBT" id="lgbt-owned-check"/>
+              <label class="form-check-label" for="lgbt-owned-check">
+                🏳️‍🌈 LGBT-owned
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" onClick={handleHighlightChange} type="checkbox" value="Women" id="women-owned-check"/>
+              <label class="form-check-label" for="women-owned-check">
+                ♀ Women-owned
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" onClick={handleHighlightChange} type="checkbox" value="Eco" id="eco-friendly-check"/>
+              <label class="form-check-label" for="eco-friendly-check">
+                🌱 Eco-friendly
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" onClick={handleHighlightChange} type="checkbox" value="Community" id="community-impact-check"/>
+              <label class="form-check-label" for="community-impact-check">
+                🤲 Community impact
+              </label>
+            </div>
           </div>
           <button
             onClick={handleFormSubmit}
