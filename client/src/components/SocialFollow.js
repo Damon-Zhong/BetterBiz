5e5be5553d1142cf2476ac46342e31cb1bb22db8
Sadia Batool <sadia110@my.yorku.ca>
2020-08-04 14:06:43 -0400

    added framework for about us

--- client/src/components/SocialFollow.js
+++ client/src/components/SocialFollow.js
@@ -11,8 +11,20 @@ export default function SocialFollow() {
     <footer class="footer text-center">
     <div class="container">
       <ul class="list-inline mb-5">
-        <li class="list-inline-item">
-          <a className="facebook social" href="#!" > 
+        <li class="list-inline-item"> 
+        <h3>Connect with Us!</h3>   
+         <a href="" 
+        className="facebook social"> 
+        <FontAwesomeIcon icon={faFacebook} size="2x" /> 
+        </a> 
+        <a href="" className="twitter social"> 
+        <FontAwesomeIcon icon={faTwitter} size="2x" /> 
+        </a> 
+        <a href="" 
+        className="instagram social"> 
+        <FontAwesomeIcon icon={faInstagram} size="2x" /> 
+        </a> 
+          {/* <a className="facebook social" href="#!" > 
           <FontAwesomeIcon icon={faFacebook} size="4x" /> 
             <i class="icon-social-facebook"></i>
           </a>
@@ -27,7 +39,7 @@ export default function SocialFollow() {
           <a className="instagram social" href="#!" > 
           <FontAwesomeIcon icon={faInstagram} size="4x" /> 
             <i class="icon-social-github"></i>
-          </a>
+          </a> */}
         </li>
       </ul>
       <p class="text-muted small mb-0">Copyright &copy; BetterBiz 2020</p>
