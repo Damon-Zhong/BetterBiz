469fe6b14e6043dbced78e67958d6b626fc37e5d
Sadia Batool <sadia110@my.yorku.ca>
2020-08-05 09:14:25 -0400

    fixed links , formate , and imgs

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -14,66 +14,47 @@ const AboutUs = () => {
           </div>
 
           <h2 className="mb-5">Our Team </h2>
-        </div>   
-        <div className="row">
-          <div className="col-lg-6">
-            <a className="portfolio-item members" href="https://github.com/cestmarcel">
-              <div className="caption">
-                <div className="caption-content">
-                  <div className="h2">Marcel</div>
-                  <p className="mb-0"> 
-                  Postion A 
-                  </p>
-                  <a className="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-github"></i></a>
-
-                </div>
-              </div>
-              <img className="img-fluid" src="./images/loadingSpinner.gif" alt="100%" />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item members" href="https://github.com/Damon-Zhong">
-              <div className="caption">
-                <div className="caption-content">
-                  <div className="h2"> Damon</div>
-                  <p className="mb-0">Postion B</p> 
-                  <a className="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-github"></i></a>
-
-                </div>
-              </div>
-              <img
-                className="img-fluid"
-                src="client/src/assets/images/portfolio-2.jpg"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item members" href="https://github.com/etammao">
-              <div className="caption">
-                <div className="caption-content">
-                  <div className="h2">Etam</div>
-                  <p className="mb-0">Postion C</p> 
-                  <a className="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-github"></i></a>
-
-                </div>
-              </div>
-              <img className="img-fluid" src="img/portfolio-3.jpg" alt="" />
-            </a>
-          </div>
-          <div className="col-lg-6">
-            <a className="portfolio-item members" href="https://github.com/sadia110">
-              <div className="caption">
-                <div className="caption-content">
-                  <div className="h2">Sadia</div>
-                  <p className="mb-0"> Postion D</p> 
-                  <a className="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-github"></i></a>
-
-                </div>
-              </div>
-              <img className="img-fluid" src="img/portfolio-4.jpg" alt="" />
-            </a>
-          </div>
+        </div>     
+        <div class Name="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src= {require(process.env.PUBLIC_URL + "./team-images/default.png")} alt="" />
+                            <h4>Marcel </h4>
+                            <p class="text-muted">Lead Designer</p>
+                            <a className="btn btn-dark btn-social mx-2" href="https://github.com/cestmarcel"><i class="fab fa-github"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div> 
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                        <img class="mx-auto rounded-circle" src= {require(process.env.PUBLIC_URL + "./team-images/default.png")} alt="" />
+                            <h4>Damon</h4>
+                            <p class="text-muted">Lead Designer</p>
+                            <a className="btn btn-dark btn-social mx-2" href="https://github.com/Damon-Zhong"><i class="fab fa-github"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                            
+                        </div>
+                    </div> 
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                        <img class="mx-auto rounded-circle" src= {require(process.env.PUBLIC_URL + "./team-images/default.png")} alt="" />
+                            <h4>Etam</h4>
+                            <p class="text-muted">Lead Designer</p> 
+                            <a className="btn btn-dark btn-social mx-2" href="https://github.com/etammao"><i class="fab fa-github"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="!#"><i class="fab fa-linkedin-in"></i></a>
+                           
+                        </div>
+                    </div> 
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                        <img class="mx-auto rounded-circle" src= {require(process.env.PUBLIC_URL + "./team-images/sadia1.jpeg")} alt="" />
+                            <h4>Sadia</h4>
+                            <p class="text-muted">Lead Designer</p> 
+                            <a className="btn btn-dark btn-social mx-2" href="https://github.com/sadia110"><i class="fab fa-github"></i></a>
+                            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                            
+                        </div>
+                    </div>
         </div>
       </div> 
     </section>
