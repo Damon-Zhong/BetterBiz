5d55b2b409a8f2d29485fcd5fefcc1d130f9e6f6
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-08-05 18:32:56 -0400

    Updated landing page header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Header.css";
+import Image from "react-bootstrap/Image";
+import header from "../../images/header.png";
+import scrollDown from "../../images/scrollDown.gif";
 
 const Header = () => {
   return (
-    <header className="masthead d-flex">
-      <div className="container text-center my-auto">
-        <h1 className="mb-1">BetterBiz</h1>
-        <h3 className="mb-5">
-          <em>Support your community! </em>
-        </h3>
-        <a
-          className="btn btn-primary btn-xl js-scroll-trigger box"
-          href="#about"
-        >
-          {" "}
-          Let Go!
-        </a>
-      </div>
-      <div className="overlay"></div>
-    </header>
+    <div className="header">
+      <header className="masthead d-flex">
+        <div className="container text-center my-auto headerText">
+          <h1 className="homepage mb-3">Discover new businesses in your neighbourhood</h1>
+          <h5 className="mb-5">
+            BetterBiz is here to give visibility to underrepresented businesses. We want to help you support diverse businesses with a positive impact on their community.
+          </h5>
+          <div className="container mt-5">
+              <Image className="scrollDown" src={scrollDown} style={{margin: "0 auto", width: "100px", display: "block" }}/>
+            </div>
+          </div>
+        <div className="overlay"></div>
+      </header>
+    </div>
   );
 };
 
