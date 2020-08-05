8315bd1697e650a1203286bd38b40c8690ab9a98
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-08-05 13:29:14 -0400

    Enabled event searching and displaying function

---
+++ client/src/components/EventCard/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import moment from 'moment'
+
+const EventCard = (props) => {
+    return (
+        <div className="col-4">
+            <div className="card" style={{ width: "18rem" }}>
+              {/* image_url */}
+              <img
+                src={props.image_url}
+                className="card-img-top"
+                alt={props.image_url ? props.id : 'no image available'}
+              />
+              <div className="card-body">
+                {/* name */}
+                <h5 className="card-title">
+                  {props.name}
+                </h5>
+                {/* description */}
+                <p className="card-text">
+                  {props.description}
+                </p>
+              </div>
+              <ul className="list-group list-group-flush">
+                {/* time_start*/}
+                <li className="list-group-item">{moment(props.time_start).format('LL')}</li>
+                {/* location.city */}
+                <li className="list-group-item">{props.location.address1}</li>
+                {/* category */}
+                <li className="list-group-item">{props.category}</li>
+                {/* Free */}
+                <li className="list-group-item">{props.isFree ? 'This event is free':<a href={props.tickets_url}>Tickets available here</a>}</li>
+              </ul>
+              <div className="card-body">
+                {/* event_site_url */}
+                <a
+                  href={props.event_site_url}
+                  className="card-link"
+                >
+                  SEE DETAILS
+                </a>
+              </div>
+            </div>
+          </div>
+    )
+}
+
+export default EventCard;
