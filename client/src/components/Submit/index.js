263f143136d019ab0f54a7a40dd1e705cb48dc26
Etam <etammao@gmail.com>
2020-08-06 00:50:41 -0400

    special event css

--- client/src/components/Submit/index.js
+++ client/src/components/Submit/index.js
@@ -7,7 +7,7 @@ const Submit = () => {
       <div className="container text-center">
         <h2 className="mx-auto mb-5">
           {" "}
-          Own a business? Submit your Business and hlep grow our community!
+          Own a business? Submit your Business and help grow our community!
         </h2>
         <a className="btn btn-primary btn-xl box" href="/submitbusiness">
           Submit
