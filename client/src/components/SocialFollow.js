7faf9d5b3849ac16beffe36f75ec1b9b589f1aee
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-08-05 21:59:57 -0400

    Restyled footer

--- client/src/components/SocialFollow.js
+++ client/src/components/SocialFollow.js
@@ -13,24 +13,14 @@ export default function SocialFollow() {
     <div class="container">
       <ul class="list-inline mb-5">
         <li class="list-inline-item"> 
-        <h3>Connect with Us!</h3>   
-         <a href="" 
-        className="facebook social"> 
-        <FontAwesomeIcon icon={faFacebook} size="2x" /> 
-        </a> 
-        <a href="" className="twitter social"> 
-        <FontAwesomeIcon icon={faTwitter} size="2x" /> 
-        </a> 
-        <a href="" 
-        className="instagram social"> 
-        <FontAwesomeIcon icon={faInstagram} size="2x" /> 
-        </a> 
-        <a href="" 
-        className="github"> 
+        <h3>Find us on Github</h3>   
+        <a href="https://github.com/Damon-Zhong/BetterBiz" 
+        className="github social"> 
         <FontAwesomeIcon icon={faGithub} size="2x" /> 
         </a> 
         </li>
       </ul>
+      <p className="text-muted small mb-0">Built with ❤️ in Toronto</p>
       <p className="text-muted small mb-0">Copyright &copy; BetterBiz 2020</p>
     </div>
   </footer>
