1546875117eca25eb1ae5e0f4a8ea8e025ebce95
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-08-06 17:32:30 -0400

    Added /* endpoint

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require( 'dotenv' ).config()
-
+const mongoose = require( 'mongoose' );
 const express = require('express')
 const router = require('./app/route')
 const orm = require('./connection/orm')
@@ -7,7 +7,9 @@ const uuid = require('uuid')
 const app = express()
 const PORT = process.env.PORT || 8080
 const API_URL = process.env.NODE_ENV === 'production' ? 'https://betterbiz01.herokuapp.com/' : 'http://localhost:8080'
-
+//connect to database
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/betterbiz',
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -27,6 +29,11 @@ app.use( express.static('client/build') )
 if( process.env.NODE_ENV === 'production' ){
     app.use( express.static('client/build') )
 }
+
+app.get('/*', function (req, res) {
+    console.log( `[/*] (${req.protocol}//${req.get('host')}/${req.originalUrl} -- sending file: ${__dirname}/client/build/index.html` );
+    res.sendFile(`${__dirname}/client/build/index.html`);
+});
 
 app.listen(PORT, function() {
     console.log( `Listening on port: ${PORT}` )
